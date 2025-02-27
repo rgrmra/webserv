7cad5bd1ef17b8851cbb5b3a48a9c6c997f7c68e
Roger de Moura <roger.moura@libercapital.com.br>
2025-02-27 12:07:11 -0300

    refact: wip: add fork to archive

--- src/connection/Archive.cpp
+++ src/connection/Archive.cpp
@@ -1,9 +1,40 @@
 #include "Archive.hpp"
 #include "AStream.hpp"
+#include "WebServ.hpp"
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 Archive::Archive(int fd, std::string id)
 	: AStream(fd, id) {
 
+	_default_fd = fd;
+
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, sock) == -1) {
+		perror("socketpair");
+		return;
+	}
+
+	pid = fork();
+	
+	if (pid == 0) {
+		close(sock[0]);
+
+		char *argv[] = {strdup("./index.php"), NULL};
+		execve("./index.php", argv, __environ);
+
+		close(sock[1]);
+		exit(1);
+	}
+	close(sock[1]);
+	waitpid(pid, NULL, WNOHANG);
+
+	_fd = sock[0];
+	WebServ::getInstance()->addFdToEpoll(sock[0], this);
 }
 
 Archive::Archive(const Archive &src)
--- src/connection/Archive.hpp
+++ src/connection/Archive.hpp
@@ -2,9 +2,14 @@
 #define ARCHIVE_HPP
 
 #include "AStream.hpp"
+#include <sched.h>
 #include <string>
 
 class Archive : public AStream {
+	private:
+		int _default_fd;
+		int sock[2];
+		pid_t pid;
 
 	public:
 		Archive(int fd, std::string id);
