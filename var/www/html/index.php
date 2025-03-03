a9a246e9716c6a7640d203a0ebe406e084086e34
Roger de Moura <roger.moura@libercapital.com.br>
2025-03-03 14:54:20 -0300

    refact: improve php test page

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -1,6 +1,7 @@
 #!/usr/bin/php-cgi
 <?php
-	//phpinfo()
-	$data = file_get_contents("php://input");
-	echo "received: " . $data;
+	//phpinfo();
+	echo isset($_GET["teste"]) ? $_GET["teste"] : "Parâmetro não definido";
+	//$data = file_get_contents("php://input");
+	//echo "received: " . $data;
 ?>
