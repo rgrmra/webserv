f10ab32507c5e62b719ed37613f95d307c8c7b26
Roger de Moura <roger.moura@libercapital.com.br>
2025-03-03 14:37:45 -0300

    feat: separate process requests by methods - add logic to get method - remove unused methods - change response status Not Allowed to Method Not Allowed

--- src/response/process.hpp
+++ src/response/process.hpp
@@ -9,15 +9,14 @@ class Location;
 namespace process {
 
 	void request(Connection *connection);
+	void methodGet(Connection *connection);
+	void methodPost(Connection *connection);
+	void methodDelete(Connection *connection);
 
 	bool isFile(const std::string &path);
 	bool isDirectory(const std::string &path);
 	bool isCGI(const std::string &path);
-	bool checkIndex(const Location &location, std::string &path);
-	std::string getFileExtension(std::string path);
-	Location isValidPath(Connection *connection);
-	std::string getPathFromReferer(std::string url);
-	std::string getFileFromPath(std::string path);
+	std::string checkIndex(const Location &location, std::string &path);
 
 };
 
